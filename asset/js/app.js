5f77cc662caa5b534271d7a66b18b1af2e142ee4
brayanospina2005 <ospinabrayan431@gmail.com>
2022-10-17 21:42:42 -0500

    Footer finish

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,5 +1,5 @@
 document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("card")) {
+    for(const card of document.getElementsByClassName("cardes")) {
       const rect = card.getBoundingClientRect(),
             x = e.clientX - rect.left,
             y = e.clientY - rect.top;
@@ -8,3 +8,12 @@ document.getElementById("cards").onmousemove = e => {
       card.style.setProperty("--mouse-y", `${y}px`);
     };
   }
+  const close = document.querySelector(".close");
+  const open = document.querySelector(".ham");
+  const menu = document.querySelector(".menu");
+  close.addEventListener("click", () => {
+    menu.style.visibility = "hidden";
+  });
+  open.addEventListener("click", () => {
+    menu.style.visibility = "visible";
+  });
