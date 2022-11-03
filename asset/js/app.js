2a10183fa2c410ce3d97c42bb4fb6d27577755e4
brayanospina2005 <ospinabrayan431@gmail.com>
2022-11-02 23:55:21 -0500

    Email auto

--- asset/js/app.js
+++ asset/js/app.js
@@ -1,29 +1,31 @@
 document.getElementById("cards").onmousemove = e => {
-    for(const card of document.getElementsByClassName("cardes")) {
-      const rect = card.getBoundingClientRect(),
-            x = e.clientX - rect.left,
-            y = e.clientY - rect.top;
-  
-      card.style.setProperty("--mouse-x", `${x}px`);
-      card.style.setProperty("--mouse-y", `${y}px`);
-    };
+  for (const card of document.getElementsByClassName("cardes")) {
+    const rect = card.getBoundingClientRect(),
+      x = e.clientX - rect.left,
+      y = e.clientY - rect.top;
+
+    card.style.setProperty("--mouse-x", `${x}px`);
+    card.style.setProperty("--mouse-y", `${y}px`);
+  };
+}
+const close = document.querySelector(".close");
+const open = document.querySelector(".ham");
+const menu = document.querySelector(".menu");
+close.addEventListener("click", () => {
+  menu.style.visibility = "hidden";
+});
+open.addEventListener("click", () => {
+  menu.style.visibility = "visible";
+});
+
+/* contact */
+
+$('.js-input').keyup(function () {
+  if ($(this).val()) {
+    $(this).addClass('not-empty');
+  } else {
+    $(this).removeClass('not-empty');
   }
-  const close = document.querySelector(".close");
-  const open = document.querySelector(".ham");
-  const menu = document.querySelector(".menu");
-  close.addEventListener("click", () => {
-    menu.style.visibility = "hidden";
-  });
-  open.addEventListener("click", () => {
-    menu.style.visibility = "visible";
-  });
+});
 
-  /* contact */
-
-  $( '.js-input' ).keyup(function() {
-    if( $(this).val() ) {
-       $(this).addClass('not-empty');
-    } else {
-       $(this).removeClass('not-empty');
-    }
-  });
+/* email js */
---
+++ asset/js/email.js
@@ -0,0 +1,20 @@
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+
+    btn.value = 'Sending...';
+
+    const serviceID = 'default_service';
+    const templateID = 'template_kr7hhxl';
+
+    emailjs.sendForm(serviceID, templateID, this)
+      .then(() => {
+        btn.value = 'Send Email';
+        alert('Sent!');
+      }, (err) => {
+        btn.value = 'Send Email';
+        alert(JSON.stringify(err));
+      });
+  });
